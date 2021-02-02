9657e2ae5a746a1f4477cb9f53fa6a93e7ad0194
vimaleshn98 <vimaleshn98@gmail.com>
2021-02-02 11:49:14 +0530

    First Commit from Eclipse

---
+++ MavenTestApp/src/main/java/example/App.java
@@ -0,0 +1,21 @@
+package example;
+
+import java.util.logging.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+//    	Logger logger =  LoggerFactory.getLogger();
+    	
+    }
+    public  void add( )
+    {
+        System.out.println( "sum" );
+    }
+}
